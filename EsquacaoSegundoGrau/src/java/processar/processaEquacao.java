5d371321eb62ab56dd2fa671c6151fa7413f8e00
Luana <116681779+lueinos@users.noreply.github.com>
2022-10-25 18:42:30 -0300

    Add files via upload

---
+++ EsquacaoSegundoGrau/src/java/processar/processaEquacao.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package processar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "processaEquacao", urlPatterns = {"/processaEquacao"})
+public class processaEquacao extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        double delta;
+        double raiz1;
+        double raiz2;
+        double a = Double.parseDouble(request.getParameter("a"));
+        double b = Double.parseDouble(request.getParameter("b"));
+        double c = Double.parseDouble(request.getParameter("c"));
+        
+        while (a==0){
+            System.out.println("O valor de A não pode ser zero a equação é do 2º grau !\n Digite outro valor !");
+        }
+        
+        delta = ((b*b)-(-4*a*c));
+        raiz1 = 0;
+        raiz2 = 0;
+        
+        if (delta >= 0){
+            raiz1 = (-b + Math.sqrt (delta)) / ( 2*a );
+            raiz2 = (-b - Math.sqrt (delta)) / ( 2*a );
+            System.out.println("Delta vale: "+ delta);
+            System.out.println("Raiz 1 vale: "+ raiz1 + "\nRaiz 2 vale: "+ raiz2);  
+        }else{
+            System.out.println("Não há soluções que possam ser expressas com o conjunto dos números reais ");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

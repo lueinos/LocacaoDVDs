84b325b9487f3a7ca8ff0738dbb6687d17fb28a0
Luana <116681779+lueinos@users.noreply.github.com>
2022-10-25 18:47:25 -0300

    Add files via upload

---
+++ FormularioProduto/src/java/processa/ProcessaProduto.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package processa;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author User
+ */
+@WebServlet(name = "ProcessaProduto", urlPatterns = {"/ProcessaProduto"})
+public class ProcessaProduto extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         
+        String codigo = request.getParameter("codigo");
+        String descricao = request.getParameter("descricao");
+        String unidade = request.getParameter("unidade");
+        String quantidade = request.getParameter("quantidade");
+        String fabricante = request.getParameter("fabricante");
+        
+        System.out.println("Dados Produtos");
+        System.out.println("Codigo de Barras: " + codigo);
+        System.out.println("Descrição: " + descricao);
+        System.out.println("Quantidade por Embalagem: " + quantidade);
+        System.out.println("Fabricante: " + fabricante); 
+        
+         if (unidade.equals("uni")){
+            System.out.println("Unidade de Medida: Unidade");
+        }else {
+            System.out.println("Unidade de Medida: Kg");
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
